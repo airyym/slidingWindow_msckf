60f8c15bb7b504dc98b723a6685ac5db56a356d1
doomzzju <dongxuanlizju@gmail.com>
2016-12-22 19:15:18 +0800

    update the propagation part of frame

--- include/frame/frame.h
+++ include/frame/frame.h
@@ -1,15 +1,23 @@
 #ifndef FRAME_H
 #define FRAME_H
 
+#include "imu/sensorflow.h"
+#include "utils/JPLquaternion.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
+#include <memory>
+#include <set>
 
 #include <Eigen/Core>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
+
+class BodyState;
 
 /**
  * @brief Frame class, include its keypoints, pose and so on.
@@ -26,6 +34,9 @@ public:
     
     /** @brief used in filter */
     Frame(double time, const cv::Mat& image);
+    
+    /** @brief used in propagation and update */
+    Frame(const BodyState& body_state, std::list<Imu>::iterator hint_gyro, std::list<Imu>::iterator hint_accel);
     
     Frame(const Frame& other) = default;
     
@@ -47,12 +58,60 @@ public:
     double getTime() const;
     cv::Mat& getImage();
     const cv::Mat& cgetImage() const;
+    
+    std::size_t getActiveFeaturesCount() const;
+    void setActiveFeaturesCount(std::size_t i);
+    void decreaseActiveFeaturesCount(int feature_id);
+    
+    BodyState& getBodyState();
+    const BodyState& getBodyState() const;
+    
+    /**
+     * @brief Time of body state
+     */
+    double time() const;
+    
+    /** @brief Get orientation \f$ {}^G \mathbf{q}_B \f$ of this camera frame in global frame. */
+    const Quaternion& getBodyOrientationInGlobalFrame() const;
+    
+    /** @brief Get orientation \f$ {}^G \mathbf{q}_C \f$ of this camera frame in global frame. */
+    Quaternion getCameraOrientationInGlobalFrame(const Filter& filter) const;
+    
+    /** @brief Get position \f$ {}^G \mathbf{p}_B \f$ of this camera frame in global frame. */
+    const Eigen::Vector3d& getBodyPositionInGlobalFrame() const;
+    
+    /** @brief Get position \f$ {}^G \mathbf{p}_C \f$ of this camera frame in global frame. */
+    Eigen::Vector3d getCameraPositionInGlobalFrame(const Filter& filter) const;
+    
+    /** @brief Get velocity \f$ {}^G\mathbf{v}_B \f$ of this camera frame in global frame. */
+    const Eigen::Vector3d& getBodyVelocityInGlobalFrame() const;
+    
+    Quaternion getRotationToOtherPose(const CameraPose& other, const Filter& filter) const;
+    
+    Eigen::Vector3d getPositionOfAnotherPose(const CameraPose& other, const Filter& filter) const;
+
+    void rememberFeatureId(int feature_id);
+
+    void updateWithStateDelta(const Eigen::VectorXd& delta_x);
+    
+    ImuBuffer::iterator gyroHint() const;
+    ImuBuffer::iterator accelHint() const;
+    
+    std::size_t getCameraPoseId() const;
 
 protected:
     bool is_valid_;
     double time_;
     cv::Mat image_;
     bool was_processed_;
+    
+    static std::size_t camera_pose_counter;
+    std::size_t camera_pose_id_;
+    std::set<int> feature_ids_;
+    std::size_t features_active_;
+    std::shared_ptr<BodyState> body_state_;
+    ImuBuffer::iterator hint_gyro_;
+    ImuBuffer::iterator hint_accel_;
   
     std::vector<cv::KeyPoint> keypoints_;
     cv::Mat descriptors_;
--- src/frame/frame.cpp
+++ src/frame/frame.cpp
@@ -1,10 +1,16 @@
 #include "frame/frame.h"
 
+#include "state/bodystate.h"
+#include "filter/filter.h"
+#include "utils/JPLquaternion.h"
+
+#include "exceptions/general_exception.h"
+
+#include <memory>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-
-#include "exceptions/general_exception.h"
 
 Frame::Frame() : is_valid_(false) {
 }
@@ -20,6 +26,14 @@ Frame::Frame(double time, const cv::Mat& image) : is_valid_(true) {
     time_ = time;
     image_ = toGray(image);
     was_processed_ = false;
+}
+
+Frame::Frame(const BodyState& body_state, std::list<Imu>::iterator hint_gyro, std::list<Imu>::iterator hint_accel)
+{
+    body_state_ = std::make_shared<BodyState>(body_state);
+    hint_gyro_ = hint_gyro;
+    hint_accel_ = hint_accel;
+    camera_pose_id_ = Frame::camera_pose_counter++;
 }
 
 Frame::operator bool() const {
@@ -45,6 +59,94 @@ cv::Mat& Frame::getImage() {
 
 const cv::Mat& Frame::cgetImage() const {
     return image_;
+}
+
+std::size_t Frame::getActiveFeaturesCount() const {
+    return features_active_;
+}
+
+void Frame::setActiveFeaturesCount(std::size_t i) {
+    features_active_ = i;
+}
+
+void Frame::decreaseActiveFeaturesCount(int feature_id) {
+    assert(feature_ids_.find(feature_id) != std::end(feature_ids_));
+    feature_ids_.erase(feature_ids_.find(feature_id));
+    
+    assert(features_active_ > 0);
+    features_active_ -= 1;
+}
+
+BodyState& Frame::getBodyState() {
+    return *body_state_;
+}
+
+const BodyState& Frame::getBodyState() const {
+    return *body_state_;
+}
+
+double Frame::time() const {
+    return body_state_->time();
+}
+
+const Quaternion& Frame::getBodyOrientationInGlobalFrame() const {
+    return body_state_->getOrientationInGlobalFrame();
+}
+
+Quaternion Frame::getCameraOrientationInGlobalFrame(const Filter& filter) const {
+    Quaternion q_C_B = filter.getBodyToCameraRotation();
+    Quaternion q_B_G = getBodyOrientationInGlobalFrame();
+    return q_C_B * q_B_G;
+}
+
+const Eigen::Vector3d& Frame::getBodyPositionInGlobalFrame() const {
+    return body_state_->getPositionInGlobalFrame();
+}
+
+Eigen::Vector3d Frame::getCameraPositionInGlobalFrame(const Filter& filter) const {
+    Quaternion q_G_C = getCameraOrientationInGlobalFrame(filter).conjugate();
+    Eigen::Vector3d p_B_G = body_state_->getPositionInGlobalFrame();
+    Eigen::Vector3d p_B_C = filter.getPositionOfBodyInCameraFrame();
+    return p_B_G - q_G_C.toRotationMatrix() * p_B_C;
+}
+
+const Eigen::Vector3d& Frame::getBodyVelocityInGlobalFrame() const {
+    return body_state_->getVelocityInGlobalFrame();
+}
+
+Quaternion Frame::getRotationToOtherPose(const CameraPose& other, const Filter& filter) const {
+    Quaternion q_Cto_G = other.getCameraOrientationInGlobalFrame(filter);
+    Quaternion q_Cfrom_G = getCameraOrientationInGlobalFrame(filter);
+    return q_Cto_G * q_Cfrom_G.conjugate();
+}
+
+Eigen::Vector3d Frame::getPositionOfAnotherPose(const CameraPose& other, const Filter& filter) const {
+    Quaternion q_Cfrom_G = getCameraOrientationInGlobalFrame(filter);
+    Eigen::Matrix3d R_Cfrom_G = q_Cfrom_G.toRotationMatrix();
+    Eigen::Vector3d p_Cto_G = other.getCameraPositionInGlobalFrame(filter);
+    Eigen::Vector3d p_Cfrom_G = getCameraPositionInGlobalFrame(filter);
+    return R_Cfrom_G * (p_Cto_G - p_Cfrom_G);
+}
+
+void Frame::rememberFeatureId(int feature_id) {
+    assert(feature_ids_.find(feature_id) == std::end(feature_ids_));
+    feature_ids_.insert(feature_id);
+}
+
+void Frame::updateWithStateDelta(const Eigen::VectorXd& delta_x) {
+    body_state_->updateWithStateDelta(delta_x);
+}
+
+std::list<Imu>::iterator Frame::gyroHint() const {
+    return hint_gyro_;
+}
+
+std::list<Imu>::iterator Frame::accelHint() const {
+    return hint_accel_;
+}
+
+std::size_t Frame::getCameraPoseId() const {
+    return camera_pose_id_;
 }
 
 void Frame::detectKeypointsAndDescripts(cv::Ptr< cv::ORB > detector, cv::Mat gray)
